9567067efacea3d47dd495b34bbd0fd1db8f46c4
Jim Klimov <jim@jimklimov.com>
2021-12-09 15:16:18 +0100

    [IPMPROG-1780] Use JSON.sh to prepare properly escaped strings from text files

--- tools/convert.sh
+++ tools/convert.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+SCRIPT_DIR="`dirname "$0"`"
+SCRIPT_DIR="`cd "$SCRIPT_DIR" && pwd`"
+
 usage() {
 cat << EOF
 #   Usage: convert.sh <INPUT_DIR> <OUTPUT_FILE>
@@ -27,8 +30,8 @@ convert_md_to_json() {
     n=0
     echo -n "[" > "$output_file"
     # for each file in directory (revert sort with file name without extension)
-    # caution: 2.3.0-1 must be prior then 2.3.0
-    for file in `ls $input_dir/*.md | rev | cut -d . -f 2- | rev | sort -r`
+    # caution: 2.3.0-1 must be prior to 2.3.0
+    for file in `ls -1 "$input_dir"/*.md | rev | cut -d . -f 2- | rev | sort -r`
     do
         version="$(basename "$file")"
         echo "Find $version ($file)"
@@ -42,12 +45,11 @@ convert_md_to_json() {
         echo "${version}\"," >> "$output_file"
         echo -n $'\t\t"content": "' >> "$output_file"
 
-        # Reading each line and convert CR with "\n"
-        while read line; do
-            # Escape '"'
-            line=$(echo "$line" | sed -e 's/[\"]/\\&/g')
-            echo -n "$line\\\\n" >> "$output_file"
-        done < "${file}.md"
+        # Read each line and convert CR with "\n"
+        "$SCRIPT_DIR/../JSON.sh/JSON.sh" -Q < "${file}.md" >> "$output_file"
+        # Add literal "\n" at end of converted text, before the closing quote"
+        echo -n '\\' >> "$output_file"
+        echo -n 'n' >> "$output_file"
         echo -n $'"\n\t}' >> "$output_file"
         n=$((n+1))
     done
@@ -60,6 +62,7 @@ convert_md_to_json() {
 if [ $# -lt 2 ]; then
     echo "Bad parameters"
     usage
+    exit 1
 else
     input_dir="$1"
     output_file="$2"
